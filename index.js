cbaa5c843c18556386b1789a36807054124cdc4d
Ekaterina <makhonina.katerina@gmail.com>
2024-11-15 10:49:05 +0300

    library inquirer

---
+++ index.js
@@ -0,0 +1,25 @@
+const inquirer = require('inquirer');
+const fs = require('fs').promises;
+
+(async () => {
+  try {
+    const readFile = await fs.readFile('./topics/raccoon_flashcard_data.txt', 'utf-8');
+
+    const answer = await inquirer.default.prompt([
+      {
+        type: 'input',
+        name: 'name',
+        message: 'Как дела?',
+      },
+      {
+        type: 'list',
+        name: 'gender',
+        message: 'Выбери',
+        choices: ['один', 'два', 'три'],
+      },
+    ]);
+    console.log(answer, readFile);
+  } catch (error) {
+    console.error('Лошара', error);
+  }
+})();
